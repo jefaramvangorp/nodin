4163fc80a847bda12044fe210e0c2e5a930d6cf6
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-27 08:19:26 +0100

    Implement connection proxy class.

--- App/Boundary/ConnectionProxy.h
+++ App/Boundary/ConnectionProxy.h
@@ -1,10 +1,31 @@
-#ifndef CONNECTIONPROXY_H
-#define CONNECTIONPROXY_H
+#ifndef APP_BOUNDARY_CONNECTIONPROXY_H_
+#define APP_BOUNDARY_CONNECTIONPROXY_H_
+
+// STD.
+#include <string>
 
 class ConnectionProxy
 {
 public:
-    ConnectionProxy();
+    ConnectionProxy(const std::string& outputNodeID, int outputIndex,
+                    const std::string& inputNodeID, int inputIndex)
+        : output_node_id_(outputNodeID)
+        , input_node_id_(inputNodeID)
+        , output_index_(outputIndex)
+        , input_index_(inputIndex)
+    {}
+
+    const std::string& outputNodeID() const { return output_node_id_; }
+    int outputIndex() const { return output_index_; }
+    const std::string& inputNodeID() const { return input_node_id_; }
+    int inputIndex() const { return input_index_; }
+
+private:
+
+    std::string output_node_id_;
+    std::string input_node_id_;
+    int output_index_;
+    int input_index_;
 };
 
-#endif // CONNECTIONPROXY_H
+#endif // APP_BOUNDARY_CONNECTIONPROXY_H_
