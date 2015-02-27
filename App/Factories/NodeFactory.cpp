0679db9be93f1f3638d9ca724f4e8fe644702743
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-27 13:40:40 +0100

    Implement NodeFactory.

--- App/Factories/NodeFactory.cpp
+++ App/Factories/NodeFactory.cpp
@@ -1,1 +1,49 @@
 
+// Includes.
+#include "App/Factories/NodeFactory.h"
+
+
+NodeFactory::NodeFactory(Delegate *delegate)
+    : delegate_(delegate)
+    , required_parameters_(delegate_->requiredParameters())
+{
+
+}
+
+NodeFactory::~NodeFactory()
+{
+
+}
+
+void NodeFactory::setParameterValue(const std::string &parameter, const std::string &value)
+{
+    parameters_[parameter] = value;
+}
+
+Node* NodeFactory::createNode()
+{
+    if (allParametersAreValid())
+    {
+        return delegate_->createNode(parameters_);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+bool NodeFactory::allParametersAreValid()
+{
+    std::vector<std::string>::const_iterator iter;
+    for (iter = required_parameters_.begin(); iter != required_parameters_.end(); ++iter)
+    {
+        const std::string& parameter_name = (*iter);
+        if (!delegate_->isValidParameter(parameter_name, parameters_[parameter_name]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
--- App/Factories/NodeFactory.h
+++ App/Factories/NodeFactory.h
@@ -1,10 +1,42 @@
-#ifndef NODEFACTORY_H
-#define NODEFACTORY_H
+#ifndef APP_FACTORIES_NODEFACTORY_H_
+#define APP_FACTORIES_NODEFACTORY_H_
+
+// STD.
+#include <map>
+#include <vector>
+#include <string>
+
+// Forward declarations.
+class Node;
 
 class NodeFactory
 {
 public:
-    NodeFactory();
+
+    class Delegate
+    {
+    public:
+        virtual ~Delegate() {}
+        virtual std::vector<std::string> requiredParameters() = 0;
+        virtual bool isValidParameter(const std::string& parameter, const std::string& value) const = 0;
+        virtual Node* createNode(const std::map<std::string, std::string>& parameters) = 0;
+    };
+
+    NodeFactory(Delegate* delegate);
+    ~NodeFactory();
+
+    const std::vector<std::string>& requiredParameters() const { return required_parameters_; }
+    void setParameterValue(const std::string& parameter, const std::string& value);
+
+    Node* createNode();
+
+private:
+
+    bool allParametersAreValid();
+
+    Delegate* delegate_;
+    std::vector<std::string> required_parameters_;
+    std::map<std::string, std::string> parameters_;
 };
 
-#endif // NODEFACTORY_H
+#endif // APP_FACTORIES_NODEFACTORY_H_
