1a1d5bfb2c324a0def61e102c44b96221ed1ece0
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-27 19:37:14 +0100

    Use new node factories.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include "UI/mainwindow.h"
 #include "App/App.h"
 
+#include "App/Factories/AdditionNodeFactoryDelegate.h"
+#include "App/Factories/ConstantNodeFactoryDelegate.h"
+#include "App/Factories/PrinterNodeFactoryDelegate.h"
+
 // Qt.
 #include <QApplication>
 
@@ -11,6 +15,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     App app;
+    app.addNodeFactory(new AdditionNodeFactoryDelegate);
+    app.addNodeFactory(new ConstantNodeFactoryDelegate);
+    app.addNodeFactory(new PrinterNodeFactoryDelegate);
 
     MainWindow w(&app);
     w.setMinimumSize(640, 480);
