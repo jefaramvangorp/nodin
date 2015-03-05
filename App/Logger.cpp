a15d93d0b6e0a02fe7b46fc989998f4af24d4c43
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-05 13:32:37 +0100

    Implement logger class.

--- App/Logger.cpp
+++ App/Logger.cpp
@@ -1,4 +1,53 @@
+
+// Includes
 #include "Logger.h"
+
+// STD.
+#include <iostream>
+
+Logger &Logger::instance()
+{
+    static Logger logger;
+    return logger;
+}
+
+void Logger::logMessage(const std::string &message)
+{
+    fprintf(stdout, "%s\n", message.c_str());
+
+    std::vector<Delegate*>::iterator iter;
+    for (iter = delegates_.begin(); iter != delegates_.end(); ++iter)
+    {
+        (*iter)->logMessage(message);
+    }
+}
+
+void Logger::logError(const std::string &message)
+{
+    fprintf(stderr, "%s\n", message.c_str());
+
+    std::vector<Delegate*>::iterator iter;
+    for (iter = delegates_.begin(); iter != delegates_.end(); ++iter)
+    {
+        (*iter)->logError(message);
+    }
+}
+
+void Logger::addDelegate(Logger::Delegate *delegate)
+{
+    if (delegate != nullptr)
+    {
+        delegates_.push_back(delegate);
+    }
+}
+
+void Logger::removeDelegate(Logger::Delegate *delegate)
+{
+    if (delegate != nullptr)
+    {
+        delegates_.push_back(delegate);
+    }
+}
 
 Logger::Logger()
 {
--- App/Logger.h
+++ App/Logger.h
@@ -1,10 +1,33 @@
-#ifndef LOGGER_H
-#define LOGGER_H
+#ifndef APP_LOGGER_H_
+#define APP_LOGGER_H_
+
+// STD.
+#include <vector>
 
 class Logger
 {
 public:
+
+    class Delegate
+    {
+    public:
+        virtual ~Delegate() {}
+        virtual void logMessage(const std::string &message) {}
+        virtual void logError(const std::string &message) {}
+    };
+
+    static Logger& instance();
+
+    void logMessage(const std::string& message);
+    void logError(const std::string& message);
+
+    void addDelegate(Delegate* delegate);
+    void removeDelegate(Delegate* delegate);
+
+private:
     Logger();
+
+    std::vector<Delegate*> delegates_;
 };
 
 #endif // LOGGER_H
