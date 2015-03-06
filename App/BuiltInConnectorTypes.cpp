4b0b4175bf722c9ba3aa03638020af2e01abdac6
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-06 22:59:06 +0100

    Add class with constants for built-in connector types.

---
+++ App/BuiltInConnectorTypes.cpp
@@ -0,0 +1,7 @@
+
+// Includes.
+#include "App/BuiltInConnectorTypes.h"
+
+const std::string BuiltInConnectorTypes::ANY = "any";
+const std::string BuiltInConnectorTypes::FLOAT = "float";
+const std::string BuiltInConnectorTypes::TEXT_LINES = "text_lines";
---
+++ App/BuiltInConnectorTypes.h
@@ -0,0 +1,17 @@
+#ifndef APP_BUILTINCONNECTORTYPES_H_
+#define APP_BUILTINCONNECTORTYPES_H_
+
+// STD.
+#include <string>
+
+class BuiltInConnectorTypes
+{
+public:
+
+    static const std::string ANY;
+    static const std::string FLOAT;
+    static const std::string TEXT_LINES;
+
+};
+
+#endif // APP_BUILTINCONNECTORTYPES_H_
