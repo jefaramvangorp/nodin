339d770923a228f0e3ccaf114d2a61f0045b78b3
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-10 07:44:31 +0100

    Create file for TypesWidget class.

---
+++ UI/TypesWidget.cpp
@@ -0,0 +1,5 @@
+#include "TypesWidget.h"
+
+TypesWidget::TypesWidget()
+{
+}
---
+++ UI/TypesWidget.h
@@ -0,0 +1,10 @@
+#ifndef TYPESWIDGET_H
+#define TYPESWIDGET_H
+
+class TypesWidget
+{
+public:
+    TypesWidget();
+};
+
+#endif // TYPESWIDGET_H
