fff3d1e8540e50d7af9222b1422a75e297a0be75
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-10 07:37:48 +0100

    Copy ParametersDialog implementation to new file.

--- UI/ParametersDialog.cpp
+++ UI/ParametersDialog.cpp
@@ -1,5 +1,65 @@
-#include "ParametersDialog.h"
 
-ParametersDialog::ParametersDialog()
+// Includes.
+#include "UI/ParametersDialog.h"
+
+// Qt.
+#include <QLabel>
+#include <QString>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
+ParametersDialog::ParametersDialog(const std::vector<std::string> &parameters, QWidget *parent)
+    : QDialog(parent)
 {
+    QGridLayout* param_layout = new QGridLayout;
+
+    for (int i = 0; i < (int)parameters.size(); ++i)
+    {
+        std::string parameter = parameters.at(i);
+        QLabel* parameter_label = new QLabel(QString::fromStdString(parameter));
+        QLineEdit* parameter_field = new QLineEdit;
+
+        param_layout->addWidget(parameter_label, i, 0);
+        param_layout->addWidget(parameter_field, i, 1);
+
+        parameter_fields_.insert(parameter, parameter_field);
+    }
+
+    QPushButton* ok_button = new QPushButton(tr("Ok"));
+    connect(ok_button, &QPushButton::clicked, this, &QDialog::accept);
+    QPushButton* cancel_button = new QPushButton(tr("Cancel"));
+    connect(cancel_button, &QPushButton::clicked, this, &QDialog::reject);
+
+    ok_button->setFocus();
+
+    QHBoxLayout* button_layout = new QHBoxLayout;
+    button_layout->addStretch();
+    button_layout->addWidget(ok_button);
+    button_layout->addWidget(cancel_button);
+    button_layout->addStretch();
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addLayout(param_layout);
+    layout->addLayout(button_layout);
+    setLayout(layout);
+}
+
+std::map<std::string, std::string> ParametersDialog::parameterValues() const
+{
+    std::map<std::string, std::string> result;
+
+    foreach (std::string param, parameter_fields_.keys())
+    {
+        result[param] = parameter_fields_.value(param)->text().toStdString();
+    }
+
+    return result;
+}
+
+std::string ParametersDialog::valueForParameter(const std::string &parameter) const
+{
+    return parameter_fields_[parameter]->text().toStdString();
 }
--- UI/ParametersDialog.h
+++ UI/ParametersDialog.h
@@ -1,10 +1,32 @@
-#ifndef PARAMETERSDIALOG_H
-#define PARAMETERSDIALOG_H
+#ifndef UI_PARAMETERSDIALOG_H_
+#define UI_PARAMETERSDIALOG_H_
 
-class ParametersDialog
+// Qt.
+#include <QDialog>
+#include <QMap>
+
+// STD.
+#include <vector>
+#include <string>
+#include <map>
+
+// Forward declarations.
+class QLineEdit;
+
+class ParametersDialog : public QDialog
 {
 public:
-    ParametersDialog();
+
+    ParametersDialog(const std::vector<std::string>& parameters, QWidget* parent = 0);
+
+    std::map<std::string, std::string> parameterValues() const;
+
+    std::string valueForParameter(const std::string& parameter) const;
+
+private:
+
+    QMap<std::string, QLineEdit*> parameter_fields_;
+
 };
 
-#endif // PARAMETERSDIALOG_H
+#endif // UI_PARAMETERSDIALOG_H_
