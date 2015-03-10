21302055160d6224ebd16f701abb34f28a5fcefc
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-10 07:29:53 +0100

    Add first lua script.

---
+++ Scripts/Lua/node.lua
@@ -0,0 +1,32 @@
+
+function split(str, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+
+    local t = {} ; i = 1
+    for str in string.gmatch(str, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
+
+name = "Add"
+num_inputs = 2
+num_outputs = 1
+input_types = { "float", "float" }
+output_types = { "float" }
+
+function evaluateForOutput(inputs, outputIndex)
+    local input = split(inputs, ",")
+    return input[1] + input[2]
+end
+
+required_parameters = {"lua_script_filename"}
+function validateParameter(name, value)
+    return name == "lua_script_filename"
+end
+
+
