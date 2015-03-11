84f17dc590846870ab62553fbc83c45aa5b8fd7d
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-11 08:29:22 +0100

    Update lua script to work with array of inputs, rather than comma-separated string. Also removed split function which is not needed.

--- NodeScripts/Lua/add.lua
+++ NodeScripts/Lua/add.lua
@@ -1,27 +1,12 @@
 
-function split(str, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-
-    local t = {} ; i = 1
-    for str in string.gmatch(str, "([^"..sep.."]+)") do
-        t[i] = str
-        i = i + 1
-    end
-    return t
-end
-
-
 name = "Add"
 num_inputs = 2
 num_outputs = 1
 input_types = { "float", "float" }
 output_types = { "float" }
 
-function evaluateForOutput(inputs, outputIndex)
-    local input = split(inputs, ",")
-    return input[1] + input[2]
+function evaluateForOutput(outputIndex)
+    return ndn_inputs.float(0) + ndn_inputs.float(1)
 end
 
 required_parameters = {}
