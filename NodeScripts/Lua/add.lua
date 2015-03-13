75991a675d74db12ec972164d4389b8494c7c832
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-13 22:36:49 +0100

    Remove documentation.

--- NodeScripts/Lua/add.lua
+++ NodeScripts/Lua/add.lua
@@ -1,16 +1,10 @@
 
--- The name or type of the node (must be unique!).
 name = "Add"
-
--- The number of inputs and outputs the node has.
 num_inputs = 2
 num_outputs = 1
-
--- The types associated to the inputs/outputs (the size of each array must match the corresponding number given above).
 input_types = { "float", "float" }
 output_types = { "float" }
 
--- Evaluates the value for the output with given index. This function performs the actual logic of the node.
 function evaluateForOutput(outputIndex)
     return ndn_inputs.float(0) + ndn_inputs.float(1)
 end
