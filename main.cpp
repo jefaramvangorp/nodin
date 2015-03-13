3593c0dd67e8a1771c31f3db443cbd6851b27227
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-03-13 22:27:22 +0100

    Remove PrinterNFDelegate, has become redundant due to logger Lua node.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include "UI/mainwindow.h"
 #include "App/App.h"
 
-#include "App/Factories/PrinterNFDelegate.h"
 #include "App/Factories/TextFileNFDelegate.h"
 
 #include "System/QtBasedFileSystem.h"
@@ -25,7 +24,6 @@ int main(int argc, char *argv[])
     QtBasedFileSystem file_system;
 
     App app(&file_system);
-    app.addNodeFactory(new PrinterNFDelegate);
     app.addNodeFactory(new TextFileNFDelegate);
     app.loadScriptNodes("Nodin/NodeScripts/Lua");
 
